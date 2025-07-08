2ac28208401aa438fc48200c0263720d096430b1
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-07-08 09:46:14 +0200

    subo requirements.txt

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/saludo/<ismael>', methods=['GET'])
+@app.route('/saludo/<nombre>', methods=['GET'])
 def saludo(nombre):
     mensaje = f'Hola {nombre}'
     return jsonify({'mensaje': mensaje})
@@ -13,19 +13,6 @@ if __name__ == '__main__':
     app.run
 
     
-"""
-Explicacion: 
-Se crea una app.py de la aplicación Flask, utilizando este framework flask... lo que permite configurar 
-un entorno de desarrollo, 
-- Se define un endpoint `/saludo/<nombre>` que acepta solicitudes GET.
-- La función `saludo` toma el parámetro `nombre` de la URL y devuelve un JSON con un mensaje de saludo.
-- Se utiliza el método `jsonify` para devolver un JSON con el mensaje.
-
-Ejecución:
-   Bueno, mi querido profesor...te dejo el requirements.txt por si acaso.
-   pero tienes que instalar 
-
-"""
 
 
 
